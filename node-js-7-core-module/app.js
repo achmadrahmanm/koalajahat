fdf39a499461b3a22902e7ceead679a5d8eaa4f6
AchmadRahmanM <achmadrahmanm@gmail.com>
2022-11-19 21:51:09 +0700

    first stuck ES6 im_ex module

--- node-js-7-core-module/app.js
+++ node-js-7-core-module/app.js
@@ -1,20 +1,59 @@
-const {tulisPertanyaan, simpanContact} = require('./contacts') 
+import _yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import * as simpanContact from "./contacts.js";
+const yargs = _yargs(hideBin(process.argv));
 
-const main =async() => {
-    const nama = await tulisPertanyaan('Nama kontak baru : ');
-    const email = await tulisPertanyaan('Email : ');
-    const noHP = await tulisPertanyaan('Nomer Hp : ');
+yargs.command({
+    command: 'add',
+    describe: 'menambahkan nama kontak',
+    builder : {
+        nama : {
+            describe : 'Nama',
+            demandOption : true,
+            type : 'string'
+        },
+        email : {
+            describe : 'Email',
+            demandOption : false,
+            type : 'string'
+        },
+        noHP : {
+            describe : 'Nomer Handphone',
+            demandOption : true,
+            type : 'string'
+        },
+    },
+    handler(argv){
+        simpanContact( argv.nama, argv.email, argv.noHP);
+        // const contact = {
+        //     nama :argv.nama, 
+        //     email:argv.email, 
+        //     noHP:argv.noHP
+        // }
+    }
+});
 
-    simpanContact(nama, email, noHP);
-}
+// yargs.command('add', 'Menambahkan kontak baru', () => {}, (argv) => {
+//     console.log(argv.nama);
+// });
+yargs.parse();
 
-main();
+/////////////////////////////////////  OLDER VERSION WITHOUT ARGUMENT //////////////////////////////////////////////////
+// const {tulisPertanyaan, simpanContact} = require('./contacts') 
+
+// const main =async() => {
+//     const nama = await tulisPertanyaan('Nama kontak baru : ');
+//     const email = await tulisPertanyaan('Email : ');
+//     const noHP = await tulisPertanyaan('Nomer Hp : ');
+
+//     simpanContact(nama, email, noHP);
+// }
+
+// main();
 
 
 
-/////////////////////////////////////  OLDER VERSION  //////////////////////////////////////////////////
-
-//pertanyaan callback hell
+/////////////////////////////////////  OLDER VERSION WITH callback hell  //////////////////////////////////////////////////
 // rl.question('Masukan nama : ', (nama) => {
 //     rl.question('masukan usia : ', (usia) => {
 //         const objContact = {nama, usia};
--- node-js-7-core-module/contacts.js
+++ node-js-7-core-module/contacts.js
@@ -1,9 +1,11 @@
-const fs = require('fs');
-const readline = require('readline');
-const rl = readline.createInterface({
-    input:process.stdin,
-    output:process.stdout,
-})
+import * as fs from 'fs';
+import chalk from 'chalk';
+
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input:process.stdin,
+//     output:process.stdout,
+// })
 
 const directoryPath = './data';
 const directoryContacts = './data/contacts.json';
@@ -14,13 +16,13 @@ if(!fs.existsSync(directoryPath)){
     fs.writeFileSync(directoryContacts, '[]', 'utf-8');
 }
 
-const tulisPertanyaan = (pertanyaan) =>{
-    return new Promise((resolve, reject) =>{
-        rl.question(pertanyaan, (input) => {
-            resolve(input);
-        });
-    });
-}
+// const tulisPertanyaan = (pertanyaan) =>{
+//     return new Promise((resolve, reject) =>{
+//         rl.question(pertanyaan, (input) => {
+//             resolve(input);
+//         });
+//     });
+// }
 
 const simpanContact = (nama, email,noHP) =>{
     const objContact = {nama, email,noHP };
@@ -29,9 +31,16 @@ const simpanContact = (nama, email,noHP) =>{
     contacts.push(objContact);
     fs.writeFileSync('data/contacts.json',JSON.stringify(contacts));
 
-    console.log(`Terimakasih ${nama} / ${email} / ${noHP} telah mengisi data !`);
-    console.log( fs.readFileSync('data/contacts.json', 'utf-8'));
-    rl.close();
+    const duplikat = contacts.find((objContact) => objContact.nama === nama);
+    if(duplikat){
+        console.log(chalk.red.inverse.bold('Contact sudah terdaftar, duplikat'));
+        return false;
+    }
+    // console.log(`Terimakasih ${nama} / ${email} / ${noHP} telah mengisi data !`);
+    // console.log( fs.readFileSync('data/contacts.json', 'utf-8'));
+    // rl.close();
 }
 
-module.exports = {tulisPertanyaan, simpanContact}
+// export default simpanContact;
+module.exports.simpanContact = simpanContact;
+// module.exports = {tulisPertanyaan, simpanContact}
